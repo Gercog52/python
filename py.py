0132beb22a543048165baea92b6a621e3f3a3a16
Gercog52 <n@mail.ru>
2020-01-11 21:19:30 +0800

    debag

--- py.py
+++ py.py
@@ -2,7 +2,7 @@ def longest_slide_down(pyramid):
     buf = [
             [
                 [
-                    pyramid[0][0]
+                    pyramid[0][0],
                 ]
             ]
           ]
@@ -25,22 +25,22 @@ def longest_slide_down(pyramid):
                     currentArr.append(buf[t-1][r][n]+pyramid[t][r])
                 #print(currentArr,'|')
                 buf[t][r]=currentArr
-            elif (r==len(pyramid[t])-1) and (t!=1):
+            elif (r==len(pyramid[t])-1):
                 print('->',buf[t-1][r-1])
                 currentArr = []
                 for n in range(0,len(buf[t-1][r-1])):
                     print('!')
                     currentArr.append(buf[t-1][r-1][n]+pyramid[t][r])
                 print(currentArr,'<-')
-                buf[t][r-1] = currentArr
-            #else: 
-            #    currentArr = []
-            #    for n in range(0,len(buf[t-1][r])):
-            #        currentArr.append(buf[t-1][r][n]+pyramid[t][r])
-            #    for n in range(0,len(buf[t-1][r-1])):
-            #        currentArr.append(buf[t-1][r-1][n]+pyramid[t][r])
-            #    buf[t][r] = currentArr
+                buf[t][r] = currentArr
+            else: 
+                currentArr = []
+                for n in range(0,len(buf[t-1][r])):
+                    currentArr.append(buf[t-1][r][n]+pyramid[t][r])
+                for n in range(0,len(buf[t-1][r-1])):
+                    currentArr.append(buf[t-1][r-1][n]+pyramid[t][r])
+                buf[t][r] = currentArr
     print(buf[len(buf)-1])
-                    
+            
     return ''
 longest_slide_down([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]])
